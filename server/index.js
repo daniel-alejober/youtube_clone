853a06c181d023c5b003bc7a93b288bd24672968
Daniel Alejo <daniel-alejo94@hotmail.com>
2023-02-14 17:22:19 -0600

    routes auth

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 import express from "express";
 import * as dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import connectDB from "./db/connectDB.js";
+import userRoutes from "./routes/users.js";
+import commentRoutes from "./routes/comments.js";
+import videoRoutes from "./routes/videos.js";
+import authRoutes from "./routes/auth.js";
 
 dotenv.config();
 const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/v1/auth", authRoutes);
+app.use("/api/v1/users", userRoutes);
+app.use("/api/v1/comments", commentRoutes);
+app.use("/api/v1/videos", videoRoutes);
 
 const startServer = async () => {
   try {
